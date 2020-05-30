38db179d895aaf8ae8455062ac1a0874b817aa8d
Nguyen Dai <ngtrdai290197@gmail.com>
2020-05-31 00:27:42 +0700

    chore: add user module into root module | update and add new for define constant

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './common/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest'),
     SharedModule,
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/common/definition/constants.ts
+++ src/common/definition/constants.ts
@@ -1,11 +1,13 @@
-export const CONSTATNS = {
+export const Constants = {
   GITHUB: {
     clientID: '8c778b22dac009f5ae4d',
     clientSecret: 'd4a240434cd481960bd40760b5d59e9b282593bb',
   },
+
   LINKED_IN: {
-    clientID: '', // TODO: not yet
-    clientSecret: '', // TODO: not yet
+    clientID: 'somethings', // TODO: not yet
+    clientSecret: 'somethings', // TODO: not yet
   },
-  SECRET_KEY: 'lsanldnaldkaldnlaskn',
+
+  SECRET_KEY: 'somethingswithcode',
 };
